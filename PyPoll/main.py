c132319df7b610cd4b60a605baf6ef097f11c4a5
Jordan Cline <jordancline@Jordans-MacBook-Pro.local>
2020-11-10 10:56:18 -0500

    Updating whatever progress was made.

---
+++ PyPoll/main.py
@@ -0,0 +1,9 @@
+
+# bringing in necessities for csv module
+import os
+import csv
+
+#opening and reading proper csv, establishing output file as well
+data_csv = os.path.join("Resources","election_data.csv")
+analysis_txt = os.path.join("Analysis","election_analysis.txt")
+
