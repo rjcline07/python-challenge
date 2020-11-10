ee9c8dba6f3bab01cc30979a0e70403fb701f910
Jordan Cline <jordancline@Jordans-MacBook-Pro.local>
2020-11-10 11:03:23 -0500

    updated progress with reading in data, creating candidate list, adding to count.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,3 +7,32 @@ import csv
 data_csv = os.path.join("Resources","election_data.csv")
 analysis_txt = os.path.join("Analysis","election_analysis.txt")
 
+# Total Vote Count and Candidate Options
+total_votes = 0
+candidates = []
+candidate_votes = {}
+
+#Reading in CSV
+
+with open(data_csv) as election_data:
+    reader = csv.reader(election_data)
+
+
+    #Header
+    header = next(reader)
+
+    #setting up for loop
+    for row in reader:
+        print(". ", end = ""),
+        total_votes += 1
+        #pulling candidate name
+        candidate_name = row[2]
+
+#making sure new candidates are added to list
+        if candidate_name not in candidates:
+            candidates.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        
+        #adding vote to candidate count
+        candidate_votes[candidate_name] += 1
+
